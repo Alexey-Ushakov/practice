e81e32615381bbc7bbfd0453393bc6fc24057d10
AlexeyUshakov <alexeyushakov42@gmail.com>
2021-06-07 13:22:56 +0300

    Добавил операторы: суммы, умножения,деление на цело и остаток от деления

---
+++ operators/operators.py
@@ -0,0 +1,17 @@
+def sum(a,b):
+    return a + b
+print(sum(2,2))
+
+def multyply(a,b):
+    return a * b
+
+print(multyply(16,6))
+
+def delenienacelo(a,b):
+    return a // b
+print(delenienacelo(20,3))
+
+def deleniesostatkom(a,b):
+    return a % b
+print(deleniesostatkom(253,16))
+
